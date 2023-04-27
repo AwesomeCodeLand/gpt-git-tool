400b1ad5df85587bc1012b9d2b49713ba7d1139d
andy <ztao8607@gmail.com>
2023-04-27 22:28:00 +0800

    Adding version command
    --cli/command.go--
    summary: Adding version command
    files: cli/command.go
    commit:  Add version command to ggt CLI tool
    --command/version.go--
    summary: Add version command function
    files: command/version.go
    commit:  Add the Version() function to display version and commit information in the CLI interface

---
+++ command/version.go
@@ -0,0 +1,21 @@
+package command
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli"
+)
+
+var version string
+var commit string
+
+func Version() cli.Command {
+	return cli.Command{
+		Name:  "version",
+		Usage: "show the version",
+		Action: func(c *cli.Context) error {
+			fmt.Printf("Version: %s %s\n", version, commit)
+			return nil
+		},
+	}
+}
